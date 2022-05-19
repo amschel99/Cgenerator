ca2700f0ac13e97f52b6383bb449f415d73dafea
amschell <100097727+amschell@users.noreply.github.com>
2022-05-19 08:21:59 +0300

    separated different components into different files

---
+++ dropdowncss.mjs
@@ -0,0 +1,30 @@
+import { db } from "./sitegen.mjs";
+let darknav
+const getDark= ()=>{
+    const promise = new Promise((resolve, reject) => {
+        db.query("SELECT dropdowncss from navcss", (err, res, fields) => {
+            if (err) reject(err);
+
+            resolve(res);
+        });
+    });
+    return promise;
+}
+//async
+ const wait=async()=> {
+
+    try{
+        const data = await getDark();
+        //console.log(data)
+        darknav=data
+        
+       // console.log(darknav)
+        return darknav
+    } catch(e) {
+        console.log(e);
+    }
+}
+
+ await wait()
+ export default darknav[0].dark
+//console.log(  darknav[0].dark)
